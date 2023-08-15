a9af187c06cd515d7851e09951b0acd9a88fc46c
Alexia <64351595+VeronicaAlexia@users.noreply.github.com>
2023-08-15 11:51:44 +0800

    ~

--- export.go
+++ export.go
@@ -23,14 +23,44 @@ func NewClient() *Client {
 	return client
 }
 
-func Get(pointPath string, params any) HttpResultInterface {
-	return NewClient().GetMethod().UrlSite(pointPath).Query(params).NewRequest()
+func Get(pointPath string, params any, headers map[string]interface{}) HttpResultInterface {
+	response := NewClient().GetMethod().UrlSite(pointPath).Query(params)
+	if headers != nil {
+		response.Headers(headers)
+	}
+	for i := 0; i < 3; i++ {
+		result := response.NewRequest()
+		if result != nil {
+			return result
+		}
+	}
+	return nil
 }
 
-func Post(pointPath string, params any) HttpResultInterface {
-	return NewClient().PostMethod().UrlSite(pointPath).Query(params).NewRequest()
+func Post(pointPath string, params any, headers map[string]interface{}) HttpResultInterface {
+	response := NewClient().PostMethod().UrlSite(pointPath).Query(params)
+	if headers != nil {
+		response.Headers(headers)
+	}
+	for i := 0; i < 3; i++ {
+		result := response.NewRequest()
+		if result != nil {
+			return result
+		}
+	}
+	return nil
 }
 
-func Put(pointPath string, params any) HttpResultInterface {
-	return NewClient().PutMethod().UrlSite(pointPath).Query(params).NewRequest()
+func Put(pointPath string, params any, headers map[string]interface{}) HttpResultInterface {
+	response := NewClient().PutMethod().UrlSite(pointPath).Query(params)
+	if headers != nil {
+		response.Headers(headers)
+	}
+	for i := 0; i < 3; i++ {
+		result := response.NewRequest()
+		if result != nil {
+			return result
+		}
+	}
+	return nil
 }
